cd5b685927a401f8d302742e76ddef9336f8ef4d
mmrichmond6 <mmrichmond6@gmail.com>
2022-08-27 18:00:42 -0500

    Five Tests Completed

--- BalancedBracketsTests/BalancedBracketsTests.cs
+++ BalancedBracketsTests/BalancedBracketsTests.cs
@@ -9,11 +9,33 @@ namespace BalancedBracketsTests
         // TODO: Add tests to this file.
 
         [TestMethod]
-        public void EmptyTest()
+        public void EmptyTest1()
         {
             Assert.AreEqual(true, true);
         }
 
+        [TestMethod]
+        public void OnlyBracketsReturnsTrue2()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[]"));
+        }
 
+        [TestMethod]
+        public void OnlyFrontBracketReturnsFalse3()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("["));
+        }
+
+        [TestMethod]
+        public void BracketsWithStringInMiddleReturnsTrue4()
+        {
+            Assert.IsTrue(BalancedBrackets.HasBalancedBrackets("[String]"));
+        }
+
+        [TestMethod]
+        public void OnlyBackBracketReturnsFalse5()
+        {
+            Assert.IsFalse(BalancedBrackets.HasBalancedBrackets("]"));
+        }
     }
 }
